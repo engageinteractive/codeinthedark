45bf70d3103110538258c0e85058014fc6c38310
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2016-04-14 10:59:39 +0100

    no need for gulp

---
+++ README.md
@@ -0,0 +1,6 @@
+# Code in the Dark
+
+1. `npm install`
+2. `npm start`
+3. Open the `src` directory in Atom (with any community packages disabled)
+4. Code
---
+++ bs-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	files:  [
+		'src/index.html',
+		'src/main.css',
+	],
+	notify: false,
+	open:   false,
+	port:   5757,
+	server: 'src'
+};
