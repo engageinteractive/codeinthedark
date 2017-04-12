7f75f53bcce269bb4621e091d8f22c71d43b8ade
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2017-04-12 09:07:17 +0100

    Updates Browsersync config.
    Open on start.

--- bs-config.js
+++ bs-config.js
@@ -1,12 +1,7 @@
 module.exports = {
 	files:  ['src/index.html'],
 	notify: false,
-	open:   false,
 	port:   5757,
 	server: ['boilerplate', 'src'],
-	ghostMode: {
-		clicks: false,
-		forms: false,
-		scroll: false
-	}
+	ghostMode: false,
 };
