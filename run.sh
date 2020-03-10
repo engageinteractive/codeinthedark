1c58c45f4cb46fd1c2ef258454160d73081d8123
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2020-03-10 15:24:44 +0000

    Tidy up

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 COLOUR_GREEN="\x1b[32;01m"
-COLOUR_RED="\x1b[31;01m"
 COLOUR_RESET="\x1b[39;49;00m"
 
-CITD_IP_ADDR=`ipconfig getifaddr en0`
+IP_ADDR=`ipconfig getifaddr en0`
 
-echo -e $COLOUR_GREEN"http://$CITD_IP_ADDR:8080/"$COLOUR_RESET
+# output host ip
+echo -e $COLOUR_GREEN"http://$IP_ADDR:8080/"$COLOUR_RESET
 
 # open chrome
 open http://localhost:8080/
